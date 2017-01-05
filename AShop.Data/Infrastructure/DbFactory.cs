b31f7b4fba6dc8830444938c90a8209174f328b7
A-PC\A <tranngocduy.it@gmail.com>
2017-01-06 06:43:04 +0700

    State 2

---
+++ AShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private AShopDbContext dbContext;
+
+        public AShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new AShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ AShop.Model/Models/RequriedAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AShop.Model.Models
+{
+    internal class RequriedAttribute : Attribute
+    {
+    }
+}
